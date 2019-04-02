59657313d8f5570690d6b687e807224c6a5cf826
Alice <alicescholze@outlook.com>
2019-04-01 22:27:38 -0300

    Inseridos testes

--- twitter/hashtag/tests.py
+++ twitter/hashtag/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.test.client import Client
+from .views import instantiate_twitter, search_tweets, home
 
-# Create your tests here.
+
+class HomeViewTest(TestCase):
+    def test_instantiate_twitter(self):
+        twitter = instantiate_twitter('ph1qcmgTRpAQbR9QYIQ3njL6u', 'vOR2XPM8HKZ1wNxC7QbVFZ3lMvfXsrd1hPwg9Z4d6fsPqsgAkq', '1108492892526600192-PEK9wbvbjBV4eu4AfLj5USPd6R9RFK', 'TwAMBTeErr2ZTvoKlkWwzV9CXGvtNAHaEzKeFOHWBBbnj')
+        self.assertTrue(twitter)
+
+    def test_search_tweets(self):
+        tweets = search_tweets('#dog')
+        self.assertGreater(len(tweets), 0)
+
+    def test_home(self):
+        client = Client()
+        response = client.get(path=home)
+        self.assertEqual(response.status_code, 200)
--- twitter/hashtag/views.py
+++ twitter/hashtag/views.py
@@ -29,18 +29,3 @@ def instantiate_twitter(consummer_key, consummer_secret, acess_key, acess_secret
 def search_tweets(text):
     twitter = instantiate_twitter('ph1qcmgTRpAQbR9QYIQ3njL6u', 'vOR2XPM8HKZ1wNxC7QbVFZ3lMvfXsrd1hPwg9Z4d6fsPqsgAkq', '1108492892526600192-PEK9wbvbjBV4eu4AfLj5USPd6R9RFK', 'TwAMBTeErr2ZTvoKlkWwzV9CXGvtNAHaEzKeFOHWBBbnj')
     return twitter.search(q=text)
-
-def search(request):
-    hashtags = Hashtag.objects.all()
-    resultados = None
-    for hashtag in hashtags:
-        if resultados is None:
-            resultados = search_tweets(hashtag.hashtag)
-        else:
-            resultados = resultados + search_tweets(hashtag.hashtag)
-    context = {
-        'resultados': resultados,
-        'hashtags': hashtags
-    }
-    return render(request, 'home.html', context, {'filter': hashtags})
-    #return render(request, 'home.html'), {'filter': user_filter}
